42406c7b68034828df13a6d9e0dd7964b535d6fd
chonginator <benchong1999@yahoo.com.au>
2024-07-27 12:49:56 +1000

    extract redundant computations from completers

--- main.py
+++ main.py
@@ -9,12 +9,37 @@ SUBURBS = SUBURBS_AND_ADJOINING_SUBURBS['SUBURB_NAME'].drop_duplicates()
 STREET_NAMES_WITH_SUBURBS = pd.read_csv('street-names-with-suburbs.csv', keep_default_na=False)
 WASTE_COLLECTION_DAYS_COLLECTION_DAYS = pd.read_csv('waste-collection-days-collection-days.csv')
 
+STREET_NAMES_WITH_SUBURBS.loc[:, 'STREET TYPE'] = (
+    STREET_NAMES_WITH_SUBURBS.loc[:, 'STREET TYPE'].str.upper().map(STREET_TYPE_ABBREVIATIONS)
+)
+
+STREET_NAMES_WITH_SUBURBS.loc[:, 'STREET SUFFIX'] = (
+    STREET_NAMES_WITH_SUBURBS.loc[:, 'STREET SUFFIX'].apply(
+        lambda suffix: STREET_SUFFIX_ABBREVIATIONS.get(suffix.upper(), '')
+    )
+)
+
 # print(WASTE_COLLECTION_DAYS_COLLECTION_DAYS[(WASTE_COLLECTION_DAYS_COLLECTION_DAYS["UNIT_NUMBER"].notna()) & (WASTE_COLLECTION_DAYS_COLLECTION_DAYS["HOUSE_NUMBER_SUFFIX"].notna())])
 # print(STREET_NAMES_WITH_SUBURBS["STREET TYPE"].drop_duplicates())
 # print(STREET_NAMES_WITH_SUBURBS["STREET SUFFIX"].drop_duplicates())
+def format_address(row):
+    address = ''
+    if pd.notna(row['UNIT_NUMBER']):
+        address += f"{str(int(row['UNIT_NUMBER']))}/"
+
+    address += str(row['HOUSE_NUMBER'])
+
+    if pd.notna(row['HOUSE_NUMBER_SUFFIX']):
+        address += row['HOUSE_NUMBER_SUFFIX']
+
+    address += f" {row['STREET_NAME']}, {row['SUBURB']}"
+
+    return address
 
 suburb = None
+suburb_streets = None
 street_name = None
+addresses = None
 
 def suburb_completer(text, state):
     global SUBURBS
@@ -26,33 +51,31 @@ def suburb_completer(text, state):
 
     return matches[state]
 
+street_completer_num_calls = 0
+
 def street_completer(text, state):
-    global suburb
+    global suburb, suburb_streets, street_completer_num_calls
+
+    street_completer_num_calls += 1
+    # print(f"Number of street completer calls: {street_completer_num_calls}")
 
     if not suburb:
         return None
 
-    suburb_streets = STREET_NAMES_WITH_SUBURBS[STREET_NAMES_WITH_SUBURBS['SUBURB'] == suburb]
+    # suburb_streets = STREET_NAMES_WITH_SUBURBS[STREET_NAMES_WITH_SUBURBS['SUBURB'] == suburb]
     filtered_streets = suburb_streets[suburb_streets['STREET NAME'].str.startswith(text.upper())]
-
-    filtered_streets.loc[:, 'STREET TYPE'] = (
-        filtered_streets.loc[:, 'STREET TYPE'].str.upper().map(STREET_TYPE_ABBREVIATIONS)
-    )
-
-    filtered_streets.loc[:, 'STREET SUFFIX'] = (
-        filtered_streets.loc[:, 'STREET SUFFIX'].apply(
-            lambda suffix: STREET_SUFFIX_ABBREVIATIONS.get(suffix.upper(), '')
-        )
-    )
 
     matches = filtered_streets.apply(
         lambda row: f"{row['STREET NAME']} {row['STREET TYPE']} {row['STREET SUFFIX']}".strip(),
         axis=1
     ).tolist()
 
+    # print(f"number of matches: {len(matches)}")
+
     if not matches:
         return None
 
+    # print(matches)
     return matches[state]
 
 def address_completer(text, state):
@@ -61,34 +84,34 @@ def address_completer(text, state):
     if not suburb or not street_name:
         return None
     
-    filtered_addresses = WASTE_COLLECTION_DAYS_COLLECTION_DAYS[
-        (WASTE_COLLECTION_DAYS_COLLECTION_DAYS["SUBURB"] == suburb) &
-        (WASTE_COLLECTION_DAYS_COLLECTION_DAYS["STREET_NAME"] == street_name)
-    ]
+    # filtered_addresses = WASTE_COLLECTION_DAYS_COLLECTION_DAYS[
+    #     (WASTE_COLLECTION_DAYS_COLLECTION_DAYS["SUBURB"] == suburb) &
+    #     (WASTE_COLLECTION_DAYS_COLLECTION_DAYS["STREET_NAME"] == street_name)
+    # ]
+    # print(filtered_addresses)
 
 
-    def format_address(row):
-        address = ''
-        if pd.notna(row['UNIT_NUMBER']):
-            address += f"{str(int(row['UNIT_NUMBER']))}/"
+    # def format_address(row):
+    #     address = ''
+    #     if pd.notna(row['UNIT_NUMBER']):
+    #         address += f"{str(int(row['UNIT_NUMBER']))}/"
 
-        address += str(row['HOUSE_NUMBER'])
+    #     address += str(row['HOUSE_NUMBER'])
 
+    #     if pd.notna(row['HOUSE_NUMBER_SUFFIX']):
+    #         address += row['HOUSE_NUMBER_SUFFIX']
 
-        if pd.notna(row['HOUSE_NUMBER_SUFFIX']):
-            address += row['HOUSE_NUMBER_SUFFIX']
+    #     address += f" {row['STREET_NAME']}, {row['SUBURB']}"
 
-        address += f" {row['STREET_NAME']}, {row['SUBURB']}"
-        # print(address)
+    #     return address
 
-        return address
+    # formatted_addresses = addresses.apply(format_address, axis=1)
+    matches = addresses[addresses.str.startswith(text)].tolist()
 
-    matches = filtered_addresses.apply(format_address, axis=1).tolist()
-    # print(matches)
     return matches[state]
 
 def main():
-    global suburb, street_name
+    global suburb, suburb_streets, addresses, street_name
 
     readline.parse_and_bind('tab: menu-complete')
     print('--- Brisbane Bin Chicken Offering Day ---')
@@ -96,8 +119,16 @@ def main():
     readline.set_completer(suburb_completer)
     suburb = input('What suburb do you live in? ')
 
+    suburb_streets = STREET_NAMES_WITH_SUBURBS[STREET_NAMES_WITH_SUBURBS['SUBURB'] == suburb]
+
     readline.set_completer(street_completer)
     street_name = input('What street do you live in? ')
+
+    addresses = WASTE_COLLECTION_DAYS_COLLECTION_DAYS[
+        (WASTE_COLLECTION_DAYS_COLLECTION_DAYS["SUBURB"] == suburb) &
+        (WASTE_COLLECTION_DAYS_COLLECTION_DAYS["STREET_NAME"] == street_name)
+    ]
+    addresses = addresses.apply(format_address, axis=1)
 
     readline.set_completer(address_completer)
     street_name = input('What is your address? ')
