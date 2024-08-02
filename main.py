1840acaf7ba0ac2c7f6ca8e2db560471c8956763
chonginator <benchong1999@yahoo.com.au>
2024-08-02 21:08:33 +1000

    extract functions from main

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from datetime import date
 import readline
 import pandas as pd
 
-def load_csv(file_path):
-    return pd.read_csv(file_path)
+def load_data(file_paths):
+    return [pd.read_csv(path) for path in file_paths]
 
 def format_address(address):
     unit_number = address['UNIT_NUMBER']
@@ -33,8 +33,28 @@ def create_completer_from(completion_matches):
         return titlecased_matches[state]
     return completer
 
-def is_yellow_recycling_bin_week_for(address):
-    waste_collection_weeks = load_csv('waste-collection-days-collection-weeks.csv')
+def get_validated_input(prompt, valid_options):
+    user_input = None
+    while user_input not in valid_options:
+        user_input = input(prompt).upper()
+
+        if user_input not in valid_options:
+            print("Please enter a valid option.")
+
+    return user_input
+
+def print_collection_message_for_address(address):
+    collection_day = address['COLLECTION_DAY']
+
+    print(f"Your bin collection day is {collection_day.title()}.")
+    
+    if is_yellow_recycling_bin_week_for_address(address):
+        print("Don't forget to take out your yellow recycling bin!")
+    else:
+        print("Don't forget to take out your green waste bin!")
+
+def is_yellow_recycling_bin_week_for_address(address):
+    waste_collection_weeks = pd.read_csv('waste-collection-days-collection-weeks.csv')
     waste_collection_weeks['WEEK_STARTING'] = pd.to_datetime(waste_collection_weeks['WEEK_STARTING'])
 
     zone = address['ZONE']
@@ -48,37 +68,32 @@ def is_yellow_recycling_bin_week_for(address):
     return False
 
 def main():
-    suburbs_and_adjoining_suburbs = load_csv('suburbs-and-adjoining-suburbs.csv')
-    waste_collection_days = load_csv('waste-collection-days-collection-days.csv')
+    suburbs_and_adjoining_suburbs, waste_collection_days = load_data([
+        'suburbs-and-adjoining-suburbs.csv',
+        'waste-collection-days-collection-days.csv'
+    ])
 
     suburbs = suburbs_and_adjoining_suburbs['SUBURB_NAME'].drop_duplicates().tolist()
 
     readline.parse_and_bind('tab: menu-complete')
     readline.set_completer_delims('')
 
-    suburb = None
-    suburb_streets = None
-    street_name = None
-    address_input = None
-
     print('--- Brisbane Bin Chicken Offering Day ---')
 
     readline.set_completer(create_completer_from(suburbs))
-
-    while suburb not in suburbs:
-        suburb = input('What suburb do you live in? ').upper()
-        if suburb not in suburbs:
-            print(f"Please enter a valid suburb.")
+    suburb = get_validated_input(
+        'What suburb do you live in? ',
+        suburbs
+    )
 
     filtered_suburb_streets = waste_collection_days[waste_collection_days['SUBURB'] == suburb]
     suburb_streets = filtered_suburb_streets['STREET_NAME'].drop_duplicates().tolist()
 
     readline.set_completer(create_completer_from(suburb_streets))
-
-    while street_name not in suburb_streets:
-        street_name = input('What street do you live in? ').upper()
-        if street_name not in suburb_streets:
-            print(f"Please enter a valid street.")
+    street_name = get_validated_input(
+        'What street do you live in? ',
+        suburb_streets
+    )
 
     filtered_addresses = waste_collection_days[
         (waste_collection_days["SUBURB"] == suburb) &
@@ -87,21 +102,11 @@ def main():
     addresses = filtered_addresses.apply(format_address, axis=1).tolist()
 
     readline.set_completer(create_completer_from(addresses))
-    
-    while address_input not in addresses:
-        address_input = input('What is your address? ').upper()
-        if address_input not in addresses:
-            print(f"Please enter a valid address")
+    address_input = get_validated_input('What is your address? ', addresses)
 
     address = filtered_addresses.iloc[addresses.index(address_input)]
-    collection_day = address['COLLECTION_DAY']
 
-    print(f"Your bin collection day is {collection_day.title()}.")
-
-    if is_yellow_recycling_bin_week_for(address):
-        print("Don't forget to take out your yellow recycling bin!")
-    else:
-        print("Don't forget to take out your green waste bin!")
+    print_collection_message_for_address(address)
 
 if __name__ == '__main__':
     main()
